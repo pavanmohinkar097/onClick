f3b2fdddff2b14b7d8fe0c3dfb84a2cd1a470ada
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-06-06 16:30:00 +0530

    06-06-2023

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect,useState } from 'react'
+
+import './App.css'
+
+/** Timers and Intervals */
+let intervalId;
+
+const App = () => {
+  const [countdown, setCountdown] = useState(0);
+
+  const start = () => {
+    intervalId = setInterval(() => {
+      setCountdown((prevState) => prevState + 1);
+    }, 1000);
+  };
+
+  const stop = () => {
+    clearInterval(intervalId);
+  };
+
+  const reset = () => {
+    stop();
+    setCountdown(0);
+    alert("Hi Ashish kaise hoo aap ???")
+  };
+
+  return (
+    <main>
+      
+      <h3>{countdown}</h3>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={reset}>Reset</button>
+    </main>
+  );
+};
+
+// Named Export
+export default App;
